6f4a086ebe96a813674bad044e72269cd2e3926e
IAftabRehman <iamaftabrehman@gmail.com>
2025-03-30 15:30:50 +0500

    Initial Commit (First Commit)

---
+++ lib/screens/signUp_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class signUp_screen extends StatefulWidget {
+  const signUp_screen({super.key});
+
+  @override
+  State<signUp_screen> createState() => _signUp_screenState();
+}
+
+class _signUp_screenState extends State<signUp_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
